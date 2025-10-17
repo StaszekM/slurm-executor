11d4bfc5e84f6b0ee3904a7daa517b6eae15dc05
Stanisław Markowski <stanislaw.markowski@pwr.edu.pl>
2025-10-17 13:01:21 +0200

    Autoclose SSH connection

--- src/slurm_executor/broker/broker.py
+++ src/slurm_executor/broker/broker.py
@@ -36,78 +36,80 @@ def slurm_task(
             )
             executor.serialize_call(func, args, kwargs)
 
-            # --- rsync codebase to remote ---
-            conn = Connection(remote)
-            remote_path = f"{workdir}/{func_name}_{int(libtime.time())}"
-            conn.run(f"mkdir -p {remote_path}")
-            print(f"[remote] Syncing to {remote}:{remote_path}")
+            with Connection(remote) as conn:
+                # --- rsync codebase to remote ---
+                remote_path = f"{workdir}/{func_name}_{int(libtime.time())}"
+                conn.run(f"mkdir -p {remote_path}")
+                print(f"[remote] Syncing to {remote}:{remote_path}")
 
-            os.system(
-                f"rsync --delete --progress -az --exclude-from=rsync-exclude.txt ./ {remote}:{remote_path}/"  # noqa: E501
-            )
-            os.system(f"rsync {call_file} {remote}:{remote_path}/call.pkl")
+                os.system(
+                    f"rsync --delete --progress -az --exclude-from=rsync-exclude.txt ./ {remote}:{remote_path}/"  # noqa: E501
+                )
+                os.system(f"rsync {call_file} {remote}:{remote_path}/call.pkl")
 
-            # --- submit job ---
-            script = f"""#!/bin/bash
-#SBATCH --partition={partition}
-#SBATCH --time={time}
+                # --- submit job ---
+                script = f"""#!/bin/bash
+    #SBATCH --partition={partition}
+    #SBATCH --time={time}
 
-set -e
-cd {remote_path}
+    set -e
+    cd {remote_path}
 
-source /usr/local/sbin/modules.sh
+    source /usr/local/sbin/modules.sh
 
-module load Python/3.12.3-GCCcore-13.3.0
+    module load Python/3.12.3-GCCcore-13.3.0
 
-uv sync
+    uv sync
 
-export PYTHONPATH={remote_path}:$PYTHONPATH
+    export PYTHONPATH={remote_path}:$PYTHONPATH
 
-echo "Running"
+    echo "Running"
 
-uv run - <<'EOF'
-from slurm_executor.executor.CloudpickleExecutor import CloudpickleExecutor
+    uv run - <<'EOF'
+    from slurm_executor.executor.CloudpickleExecutor import CloudpickleExecutor
 
-executor = CloudpickleExecutor(
-    serialize_to="call.pkl", deserialize_from="call.pkl"
-)
-executor.run()
+    executor = CloudpickleExecutor(
+        serialize_to="call.pkl", deserialize_from="call.pkl"
+    )
+    executor.run()
 
-EOF
-"""
-            job_script_name = "job.sh"
-            job_out_file = "job.out"
-            local_script = local_job_dir / job_script_name
-            local_script.write_text(script)
+    EOF
+    """
+                job_script_name = "job.sh"
+                job_out_file = "job.out"
+                local_script = local_job_dir / job_script_name
+                local_script.write_text(script)
 
-            os.system(f"rsync {local_script} {remote}:{remote_path}/{job_script_name}")
+                os.system(
+                    f"rsync {local_script} {remote}:{remote_path}/{job_script_name}"
+                )
 
-            result = conn.run(
-                f"cd {remote_path} && sbatch --output={job_out_file} {job_script_name}",
-                hide=None,
-            )
-            job_id = result.stdout.strip().split()[-1]
-            print(f"[remote] Submitted job {job_id}")
+                result = conn.run(
+                    f"cd {remote_path} && sbatch --output={job_out_file} {job_script_name}",  # noqa: E501
+                    hide=None,
+                )
+                job_id = result.stdout.strip().split()[-1]
+                print(f"[remote] Submitted job {job_id}")
 
-            # --- simple polling until job completes ---
-            while True:
-                out = conn.run(
-                    f"sacct -j {job_id} --format=State --noheader", hide=True
-                ).stdout.strip()
-                if (
-                    out.startswith("COMPLETED")
-                    or out.startswith("FAILED")
-                    or out.startswith("CANCELLED")
-                ):
-                    print(f"[remote] Job {job_id} finished: {out}")
-                    conn.run(f"cat {remote_path}/{job_out_file}", hide=None)
+                # --- simple polling until job completes ---
+                while True:
+                    out = conn.run(
+                        f"sacct -j {job_id} --format=State --noheader", hide=True
+                    ).stdout.strip()
+                    if (
+                        out.startswith("COMPLETED")
+                        or out.startswith("FAILED")
+                        or out.startswith("CANCELLED")
+                    ):
+                        print(f"[remote] Job {job_id} finished: {out}")
+                        conn.run(f"cat {remote_path}/{job_out_file}", hide=None)
 
-                    break
-                elif out.startswith("RUNNING"):
-                    print(f"[remote] Job {job_id} is still running... Cat:")
-                    conn.run(f"cat {remote_path}/{job_out_file}", hide=None)
-                libtime.sleep(5)
-            return None
+                        break
+                    elif out.startswith("RUNNING"):
+                        print(f"[remote] Job {job_id} is still running... Cat:")
+                        conn.run(f"cat {remote_path}/{job_out_file}", hide=None)
+                    libtime.sleep(5)
+                return None
 
         return wrapper
 
